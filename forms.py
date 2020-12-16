9cf82b200d09db7acaf2ec05d3d549d5298453ea
@jonathanlei <jonathanlei98@gmail.com>
2020-12-16 13:59:00 -0500

    added user edit form

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, TextAreaField
-from wtforms.validators import DataRequired, Email, Length
+from wtforms.validators import DataRequired, Email, Length, Optional, URL
 
 
 class MessageForm(FlaskForm):
@@ -22,4 +22,17 @@ class LoginForm(FlaskForm):
     """Login form."""
 
     username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[Length(min=6)])
+
+
+class UserEditForm(FlaskForm):
+    """  Form for editing user info."""
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    image_url = StringField('(Optional) Image URL', 
+                            validators=[Optional()])
+    header_image_url = StringField('(Optional) Header Image URL',
+                                   validators=[Optional()])
+    bio = StringField('Bio', validators=[DataRequired(), Length(max=160)])
+    location = StringField('Location', validators=[Optional(), Length(max=50)])
     password = PasswordField('Password', validators=[Length(min=6)])
