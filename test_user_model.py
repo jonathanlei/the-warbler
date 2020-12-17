4e533fcb2bd1aac70defeb2de3a53e6627b5ca34
@jonathanlei <jonathanlei98@gmail.com>
2020-12-16 19:58:50 -0500

    added test_user_follow function

--- test_user_model.py
+++ test_user_model.py
@@ -52,6 +52,56 @@ class UserModelTestCase(TestCase):
         db.session.add(u)
         db.session.commit()
 
+
         # User should have no messages & no followers
         self.assertEqual(len(u.messages), 0)
         self.assertEqual(len(u.followers), 0)
+        # check repr method
+        self.assertEqual(u.__repr__(),
+                         f"<User #{u.id}: testuser, test@test.com>")
+    
+    def test_user_follow(self):
+        """ Does the is_following and is_followed_by work?"""
+
+        u1 = User(
+            email="test1@test.com",
+            username="testuser1",
+            password="HASHED_PASSWORD"
+        )
+
+        u2 = User(
+            email="test2@test.com",
+            username="testuser2",
+            password="HASHED_PASSWORD"
+        )
+
+        db.session.add_all([u1, u2])
+        db.session.commit()
+
+        #u1 follows u2, u2 doesn't follow u1
+        follow = Follows(user_being_followed_id=u2.id, user_following_id=u1.id)
+        db.session.add(follow)
+        db.session.commit()
+
+        self.assertEqual(u1.is_following(u2), True)
+        self.assertEqual(u2.is_following(u1), False)
+        self.assertEqual(u2.is_followed_by(u1), True)
+        self.assertEqual(u1.is_followed_by(u2), False)
+        # delete u1 following u2
+        # print(type(u1.id), u1.id)
+        follow = Follows.query.get((u2.id, u1.id))
+        db.session.delete(follow)
+        db.session.commit()
+
+        self.assertEqual(u1.is_following(u2), False)
+        self.assertEqual(u2.is_following(u1), False)
+        self.assertEqual(u2.is_followed_by(u1), False)
+        self.assertEqual(u1.is_followed_by(u2), False)
+
+
+
+
+
+
+
+
